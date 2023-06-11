88ab0987c26d567984985ce9ca64cfda06d542e7
leangaurav <leangaurav.dev@gmail.com>
2023-06-11 08:13:58 +0000

    Deploying to gh-pages from @ code-dudes/frontend@9cf4127f6fbeec70eba17610e60bed744a9b9558 🚀

--- _next/static/p00eRCNUub7Y_17p72Weu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1df3fe956117b26e.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
